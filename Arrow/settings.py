d7c90d56b2ded125cc27b2cb746227d7dd0a0cc1
Jukesasaki <jukesasaki@gmail.com>
2020-02-27 08:33:13 +0900

    'commit--sessionOK2'

--- Arrow/settings.py
+++ Arrow/settings.py
@@ -108,6 +108,17 @@ if local_setting:
     DATABASES = {
         'default': dj_database_url.config()
     }
+else:
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': 'appDB',
+                'USER': 'postgres',
+                'PASSWORD': 'arrowmgr',
+                'HOST': '127.0.0.1',
+                'PORT': 5432,
+            }
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
