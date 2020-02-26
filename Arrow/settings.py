4cd4e75b99955c012ad001cb369c1235e96bd3e9
Jukesasaki <jukesasaki@gmail.com>
2020-02-27 02:06:13 +0900

    'commit--02/27'

--- Arrow/settings.py
+++ Arrow/settings.py
@@ -145,4 +145,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
